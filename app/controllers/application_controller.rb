b69cf8c0352c2f1e273a115336c632c318e01915
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-21 17:13:08 +0000

    Load categories in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_filter :getCategoryNav
   protect_from_forgery with: :exception
+  
+  private
+  def getCategoryNav
+      @categoryNav = Category.all
+  end
 end
