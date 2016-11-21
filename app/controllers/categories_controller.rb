1faea8f299cf4f8e57e4ffcfee223490c5e607c4
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-21 22:23:23 +0300

    Add a new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,9 +9,26 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = 'Category added'
+
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+
   def edit
   end
 
   def delete
+  end
+
+  private
+  def category_params
+    params.require(:categry).permit(:name)
   end
 end
