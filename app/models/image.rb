2b1bd554cf5f6431efca68d55a426fdfdf3977bc
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-21 20:25:57 +0000

    Upload an image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,15 @@
 class Image < ApplicationRecord
     belongs_to :category
+    
+    mount_uploader :image, ImageUploader
+    
+    before_save :update_image_attributes
+    
+    private
+    def update_image_attributes
+      if image.present? && image_changed?
+          self.image_content_type = image.file.content_type
+          self.image_file_size = image.file.size
+      end
+    end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -32,15 +32,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+   version :thumb do
+     process :resize_to_limit => [350, 260]
+   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_white_list
+    %w(jpg jpeg gif png)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
