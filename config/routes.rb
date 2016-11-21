b0de95d33ab7277c9b7943f97baf12ad7fed0e36
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-21 15:42:53 +0000

    Generate Categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
+  get 'categories/show'
+
+  get 'categories/new'
+
+  get 'categories/edit'
+
+  get 'categories/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
