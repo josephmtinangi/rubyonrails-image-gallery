845da3d92c099b817207961ece042f6785c3f0d9
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-24 19:15:06 +0000

    Force the user to redirect to the login page if the user was not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_filter :getCategoryNav
   protect_from_forgery with: :exception
+   before_action :authenticate_user!
   
   private
   def getCategoryNav
